61f39a2797f3eaf91850348888b8c1cb9b80efda
victor dronov <victordronov1@gmail.com>
2020-09-01 16:52:06 -0400

    project setup and complete?

---
+++ github-user-card/src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import axios from "axios";
+import User from "./User";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: [],
+      usersFollowers: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log("mount");
+    axios
+      .get("https://api.github.com/users/victorDronov")
+      .then((res) => {
+        this.setState({
+          user: res.data,
+        });
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // Followers
+    axios
+      .get(" https://api.github.com/users/victorDronov/followers")
+      .then((res) => {
+        let arrayOfFollowers = res.data;
+        arrayOfFollowers.forEach((persons) => {
+          this.setState({
+            usersFollowers: persons,
+          });
+          console.log(persons);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  componentDidUpdate() {
+    console.log("Update");
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div className="App">
+        <h1>Github User</h1>
+        <User
+          userDetails={this.state.user}
+          followers={this.state.usersFollowers}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ github-user-card/src/User.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+const User = (props) => {
+  const { userDetails, followers } = props;
+
+  return (
+    <div>
+      <img src={userDetails.avatar_url} />
+      <h2>Name: {userDetails.login}</h2>
+      <div>
+        <p>
+          GitHub: <a href={userDetails.html_url}>Profile</a>
+        </p>
+        <p>Bio: {userDetails.bio}</p>
+        <p>Followers: {userDetails.followers}</p>
+        <div>{followers.login}</div>
+        <p>Following: {userDetails.following}</p>
+      </div>
+    </div>
+  );
+};
+
+export default User;
